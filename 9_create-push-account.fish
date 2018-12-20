dedee7e48a07e700214c884f7d8a37a319f072ab
shinyay <shinya.com@gmail.com>
2018-12-20 20:46:15 +0900

    Add strage.admin granted to push-image accoount

--- 9_create-push-account.fish
+++ 9_create-push-account.fish
@@ -1,3 +1,7 @@
 #!/usr/bin/env fish
 
 gcloud iam service-accounts create push-image
+
+gcloud projects add-iam-policy-binding $GCP_PROJECT_ID \
+    --member serviceAccount:push-image@$GCP_PROJECT_ID.iam.gserviceaccount.com \
+    --role roles/storage.admin
